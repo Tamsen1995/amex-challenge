af74898ddca1267bf37bc4014ea081adc27e8f65
Tamsen1995 <tamanhbui95@gmail.com>
2024-03-16 14:39:48 -0400

    update rendering logic

--- amex-challenge/src/pages/peoplesDirectory.tsx
+++ amex-challenge/src/pages/peoplesDirectory.tsx
@@ -10,6 +10,7 @@ export async function getServerSideProps() {
 }
 
 export default function Home({ users }: { users: any[] }) {
+  console.log("user - > ", users[0]); // TESTING
   return (
     <div className="flex flex-wrap">
       {users.map((user, index) => (
@@ -18,10 +19,19 @@ export default function Home({ users }: { users: any[] }) {
           className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-4"
         >
           <div className="flex flex-col items-center text-center bg-gray-200 p-4 rounded-lg">
-            <h2 className="mb-2">{user.email}</h2>
+            <Image
+              src={user.picture.large}
+              alt="user"
+              width={100}
+              height={100}
+              className="rounded-full"
+            />
+            Name:{" "}
             <p>
               {user.name.title} {user.name.last}
             </p>
+            <span>Age: {user.dob.age}</span>
+            <span>City: {user.location.city}</span>
           </div>
         </div>
       ))}
