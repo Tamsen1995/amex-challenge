46a813f7e7bcced66fe8f90fb753deecabb74708
Tamsen1995 <tamanhbui95@gmail.com>
2024-03-17 14:39:22 -0400

    improve styling

--- amex-challenge/src/pages/index.tsx
+++ amex-challenge/src/pages/index.tsx
@@ -20,9 +20,9 @@ export default function Home() {
           <div className="mt-4">
             <Link
               href="/users"
-              className="px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+              className="px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-900"
             >
-              Go to People's Directory
+              Go to People&apos;s Directory
             </Link>
           </div>
         </div>
