cdc633796476a254b7faf3f6723efd809629349f
Tamsen1995 <tamanhbui95@gmail.com>
2024-03-17 14:38:29 -0400

    Change color scheme

--- amex-challenge/src/components/NavBar.tsx
+++ amex-challenge/src/components/NavBar.tsx
@@ -2,13 +2,26 @@ import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <nav className="bg-blue-500 p-4">
-      <Link href="/" className="text-white">
-        Home
-      </Link>
-      <Link href="/users" className="ml-4 text-white">
-        Users
-      </Link>
+    <nav className="bg-gradient-to-r from-blue-400 via-blue-500 to-blue-700 p-4 shadow-md">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="text-2xl font-bold text-white">
+          <Link href="/">AMEX</Link>
+        </div>
+        <div className="space-x-4">
+          <Link
+            className="text-white hover:text-gray-200 transition duration-200"
+            href="/"
+          >
+            Home
+          </Link>
+          <Link
+            className="text-white hover:text-gray-200 transition duration-200"
+            href="/users"
+          >
+            Users
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 };
