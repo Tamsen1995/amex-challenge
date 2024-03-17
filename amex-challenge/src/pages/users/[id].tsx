f2473d15b753461d003c1ce4c0bd52c1e943f3df
Tamsen1995 <tamanhbui95@gmail.com>
2024-03-17 14:14:09 -0400

    basic styling

--- amex-challenge/src/pages/users/[id].tsx
+++ amex-challenge/src/pages/users/[id].tsx
@@ -31,36 +31,35 @@ export default function UserPage({
   picture,
 }: UserPageProps) {
   return (
-    <div className="flex flex-col items-center bg-gray-200 p-4 rounded-lg">
-      <h1 className="text-2xl font-bold mb-4">User Details</h1>
-      <Image
-        src={picture}
-        alt="user"
-        width={100}
-        height={100}
-        className="rounded-full"
-      />
-      <p>
-        <span className="font-bold">ID:</span> {id}
-      </p>
-      <p>
-        <span className="font-bold">Name:</span> {name}
-      </p>
-      <p>
-        <span className="font-bold">Age:</span> {age}
-      </p>
-      <p>
-        <span className="font-bold">Address:</span> {address}
-      </p>
-      <p>
-        <span className="font-bold">Email:</span> {email}
-      </p>
-      <p>
-        <span className="font-bold">Date of Birth:</span> {dob}
-      </p>
-      <p>
-        <span className="font-bold">Phone:</span> {phone}
-      </p>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-2">
+      <div className="max-w-md w-full space-y-8 p-6 bg-white rounded-xl shadow-lg z-10">
+        <div className="grid  gap-8 grid-cols-1">
+          <div className="flex flex-col ">
+            <div className="flex flex-col sm:flex-row items-center">
+              <Image
+                src={picture}
+                alt="user"
+                width={100}
+                height={100}
+                className="rounded-full"
+              />
+              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">ID: {id}</p>
+                  <p className="text-xl font-bold text-gray-900 sm:text-2xl">
+                    Name: {name}
+                  </p>
+                  <p className="text-sm text-gray-500">Age: {age}</p>
+                  <p className="text-sm text-gray-500">Address: {address}</p>
+                  <p className="text-sm text-gray-500">Email: {email}</p>
+                  <p className="text-sm text-gray-500">Date of Birth: {dob}</p>
+                  <p className="text-sm text-gray-500">Phone: {phone}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
