54dd4c8b33fc45dcaeb8441c5732caa8172f3372
Tamsen1995 <tamanhbui95@gmail.com>
2024-03-17 14:46:54 -0400

    Better styling

--- amex-challenge/src/pages/users/[id].tsx
+++ amex-challenge/src/pages/users/[id].tsx
@@ -32,28 +32,50 @@ export default function UserPage({
 }: UserPageProps) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-2">
-      <div className="max-w-md w-full space-y-8 p-6 bg-white rounded-xl shadow-lg z-10">
-        <div className="grid  gap-8 grid-cols-1">
+      <div className="max-w-2xl w-full space-y-8 p-10 bg-white rounded-xl shadow-lg z-10">
+        <div className="grid gap-8 grid-cols-1">
           <div className="flex flex-col ">
             <div className="flex flex-col sm:flex-row items-center">
               <Image
                 src={picture}
                 alt="user"
-                width={100}
-                height={100}
+                width={200}
+                height={200}
                 className="rounded-full"
               />
               <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                 <div className="mt-2">
-                  <p className="text-sm text-gray-500">ID: {id}</p>
-                  <p className="text-xl font-bold text-gray-900 sm:text-2xl">
-                    Name: {name}
-                  </p>
-                  <p className="text-sm text-gray-500">Age: {age}</p>
-                  <p className="text-sm text-gray-500">Address: {address}</p>
-                  <p className="text-sm text-gray-500">Email: {email}</p>
-                  <p className="text-sm text-gray-500">Date of Birth: {dob}</p>
-                  <p className="text-sm text-gray-500">Phone: {phone}</p>
+                  <p className="text-3xl text-gray-900">{name}</p>
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <div className="mt-2">
+                      <p className="text-xl text-gray-600 mb-2">
+                        Age:{" "}
+                        <span className="font-medium text-gray-900">{age}</span>
+                      </p>
+                      <p className="text-xl text-gray-600 mb-2">
+                        Address:{" "}
+                        <span className="font-medium text-gray-900">
+                          {address}
+                        </span>
+                      </p>
+                      <p className="text-xl text-gray-600 mb-2">
+                        Email:{" "}
+                        <span className="font-medium text-gray-900">
+                          {email}
+                        </span>
+                      </p>
+                      <p className="text-xl text-gray-600 mb-2">
+                        Date of Birth:{" "}
+                        <span className="font-medium text-gray-900">{dob}</span>
+                      </p>
+                      <p className="text-xl text-gray-600 mb-2">
+                        Phone:{" "}
+                        <span className="font-medium text-gray-900">
+                          {phone}
+                        </span>
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
